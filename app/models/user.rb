947571a3471e6bfa065ab2fbd05c347c3d52fa22
Chenxi Wu <wuchenxi2013@gmail.com>
2017-03-23 20:18:02 +0100

    delete all

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :images
+  has_many :images, dependent: :delete_all
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
