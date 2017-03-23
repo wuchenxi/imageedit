20999ee27f6b5cce33f0146afdb0d79452cf6e29
Chenxi Wu <wuchenxi2013@gmail.com>
2017-03-23 14:19:08 +0100

    unique user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :images
   has_secure_password
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 end
